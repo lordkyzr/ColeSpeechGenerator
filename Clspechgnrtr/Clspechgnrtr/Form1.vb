fc74dfa9bdf8651d4e95da4b90b01f82148f9326
Lord Kaiser <lordkyzr@gmail.com>
2013-05-14 11:46:34 -0700

    Initial Upload

---
+++ Clspechgnrtr/Clspechgnrtr/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim beginningtext As String = TextBox1.Text
+
+        Dim textarray() As String = beginningtext.Split(" ")
+
+        Dim vowels As Integer
+        Dim vowelloc() As Integer
+        Dim charcounter As Integer = 0
+        Dim vowelarraycounter As Integer = 0
+        Dim randomnumber As New Random
+        Dim chosennum As Integer
+        Dim whichvoweltoremove As Integer = 0
+
+        For Each item In textarray
+            vowels = 0
+            charcounter = 0
+            vowelarraycounter = 0
+
+            For Each character As Char In item
+
+                If character = "a" Or character = "e" Or character = "i" Or character = "o" Or character = "u" Then
+
+                    vowels += 1
+
+                End If
+
+            Next
+
+            ReDim vowelloc(vowels)
+
+            For Each character As Char In item
+
+                If character = "a" Or character = "e" Or character = "i" Or character = "o" Or character = "u" Then
+
+                    vowelloc(vowelarraycounter) = charcounter
+                    vowelarraycounter += 1
+                End If
+
+                charcounter += 1
+            Next
+
+            'MAX NUM TO REMOVE
+
+            If vowels <> 0 Then
+
+                If vowels = 1 Then
+                    chosennum = 1
+                Else
+                    Do Until chosennum <> 0
+                        chosennum = randomnumber.Next(vowels)
+                    Loop
+
+                End If
+
+                Else
+                chosennum = randomnumber.Next(vowels)
+            End If
+
+           
+
+
+
+
+            'GETS STRING
+            Dim newtext As String = item
+            Dim removecounter As Integer = 0
+            Dim counter As Integer = 0
+
+            Do Until counter >= chosennum
+
+                newtext = newtext.Replace(newtext.Substring(vowelloc(counter) - removecounter, 1), "")
+
+                counter += 1
+                removecounter += 1
+            Loop
+
+            TextBox2.Text = TextBox2.Text & " " & newtext
+
+
+
+
+
+
+            'LAST NEXT
+        Next
+
+        If TextBox2.Text.Substring(0, 1) = " " Then
+            TextBox2.Text = TextBox2.Text.Remove(0, 1)
+        End If
+
+    End Sub
+
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        SplashScreen.Close()
+    End Sub
+End Class
---
+++ Clspechgnrtr/Clspechgnrtr/SplashScreen.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SplashScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents MainLayoutPanel As System.Windows.Forms.TableLayoutPanel
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MainLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'MainLayoutPanel
+        '
+        Me.MainLayoutPanel.BackgroundImage = Global.Clspechgnrtr.My.Resources.Resources.Cole
+        Me.MainLayoutPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.MainLayoutPanel.ColumnCount = 1
+        Me.MainLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.MainLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MainLayoutPanel.Location = New System.Drawing.Point(0, 0)
+        Me.MainLayoutPanel.Name = "MainLayoutPanel"
+        Me.MainLayoutPanel.RowCount = 1
+        Me.MainLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.MainLayoutPanel.Size = New System.Drawing.Size(496, 303)
+        Me.MainLayoutPanel.TabIndex = 0
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 2500
+        '
+        'SplashScreen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Clspechgnrtr.My.Resources.Resources.Cole
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(496, 303)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.MainLayoutPanel)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "SplashScreen"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
